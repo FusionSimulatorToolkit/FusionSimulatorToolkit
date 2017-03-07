c1f5ad25211bc92c9b625df5bc5b598fd9b759f9
Brian Haas <brianjohnhaas@gmail.com>
2017-03-07 15:59:34 -0500

    add

---
+++ benchmarking/FusionProgParsers/STARFusion_parser.pm
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Carp;
+
+
+sub parse_fusion_result_file {
+    my ($starFusion_file) = @_;
+
+    my @fusions;
+
+    open (my $fh, $starFusion_file) or die "Error, cannot open file $starFusion_file";
+    while (<$fh>) {
+        if (/^\#/) { next; }
+
+        chomp;
+
+        my ($fusion, $junction_reads, $spanning_reads, $splice_type,
+            $fusion_gene_A, $chr_coords_A,
+            $fusion_gene_B, $chr_coords_B) = split(/\t/);
+
+        my $rest;
+        ($fusion_gene_A, $rest) = split(/\^/, $fusion_gene_A);
+        ($fusion_gene_B, $rest) = split(/\^/, $fusion_gene_B);
+
+        if ($fusion_gene_A eq $fusion_gene_B) { next; } # no self-fusions
+
+        my ($chrA, $coordA, $orientA) = split(/:/, $chr_coords_A);
+        my ($chrB, $coordB, $orientB) = split(/:/, $chr_coords_B);
+
+
+        my $struct = {
+            geneA => $fusion_gene_A,
+            chrA => $chrA || ".",
+            coordA => $coordA || ".",
+
+            geneB => $fusion_gene_B,
+            chrB => $chrB || ".",
+            coordB => $coordB || ".",
+
+            span_reads => $spanning_reads,
+            junc_reads => $junction_reads,
+        };
+
+        push (@fusions, $struct);
+
+    }
+
+    close $fh;
+
+    return(@fusions);
+}
+
+
+1; #EOM
+
