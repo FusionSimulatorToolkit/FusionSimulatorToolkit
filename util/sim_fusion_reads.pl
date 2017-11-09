937a30ac68e9ca0dfce32a316487e5e9227bac8e
Brian Haas <brianjohnhaas@gmail.com>
2017-11-09 13:03:58 -0500

    more flexible parameters

--- util/sim_fusion_reads.pl
+++ util/sim_fusion_reads.pl
@@ -8,19 +8,74 @@ use Fasta_reader;
 use Nuc_translator;
 use Math::CDF;
 
+
+use Getopt::Long qw(:config posix_default no_ignore_case bundling pass_through);
+
+
+
 my $usage = "\n\n\tusage: $0 transcripts.fasta mean_frag_size st_dev\n\n";
 
-my $transcripts_fasta_file = $ARGV[0] or die $usage;
-my $mean_frag_size = $ARGV[1] or die $usage;
-my $st_dev = $ARGV[2] or die $usage;
-
-
-
+my $transcripts_fasta_file;
+my $mean_frag_size = 300;
+my $st_dev = 50;
 
 my $num_reads = 1000;
-my $read_length = 100;
+my $read_length = 75;
+my $sim_reads_outfile_prefix = "sim";
 
-my $sim_reads_outfile_prefix = "sim_short";
+my $usage = <<__EOUSAGE__;
+
+###############################################################
+#
+#  Required:
+#
+#  --fusions_fasta <string>      : target transcripts fasta file
+#
+#  Optional:
+#
+#  --mean_frag_size <int>       : mean frag size (default: $mean_frag_size)
+#
+#  --st_dev <int>               : standard deviation (default: $st_dev)
+#
+#  --num_reads <int>            : number of reads per transcript (default: $num_reads)
+#
+#  --read_length <int>          : length of reads (default: $read_length)
+#
+#  --out_prefix <string>        : output prefix for sim reads (default: $sim_reads_outfile_prefix)
+#
+###############################################################
+
+
+__EOUSAGE__
+
+    ;
+
+my $help_flag;
+
+
+&GetOptions( 'h' => \$help_flag,
+             
+             'fusions_fasta=s' => \$transcripts_fasta_file,
+             
+             'mean_frag_size=i' => \$mean_frag_size,
+             'st_dev=i' => \$st_dev,
+
+             'num_reads=i' => \$num_reads,
+             'read_length=i' => \$read_length,
+
+             'out_prefix=s' => \$sim_reads_outfile_prefix,
+    );
+
+
+if ($help_flag) {
+    die $usage;
+}
+unless ($transcripts_fasta_file) {
+    die $usage;
+}
+
+
+
 open(my $ofh_left_fq, ">${sim_reads_outfile_prefix}_1.fastq") or die $!;
 open(my $ofh_right_fq, ">${sim_reads_outfile_prefix}_2.fastq") or die $!;
 
@@ -35,19 +90,28 @@ main: {
         my $header = $seq_obj->get_header();
 
         print STDERR "-processing $acc\n";
+
+        my ($geneA, $geneB);
         
-        $acc =~ /^([^\|]+)\|[^\-]+\-\-([^\|]+)/ or die "Error, cannot parse fusion name from acc: $acc";
-        my $geneA = $1 or die "Error, didn't parse geneA from $acc";
-        my $geneB = $2 or die "Error, didn't parse geneB from $acc";
+        if ($acc =~ /^([^\|]+)\|[^\-]+\-\-([^\|]+)/) {
+            $geneA = $1;
+            $geneB = $2;
+        }
+        else {
+            ($geneA, $geneB) = split(/--/, $acc);
+        }
+        unless ($geneA && $geneB) {
+            die "Error, couldn't parse fusion gene names from $acc";
+        }
         
         my $sequence = $seq_obj->get_sequence();
         
         $header =~ /FusedAt:(\d+)/ or die "Error, cannot parse header for FusedAt:";
-        my $fusion_pos = $1 or die "Error, couldn't extract fusion position from header: $header";
+        my $fusion_pos = $1 or die "Errour, couldn't extract fusion position from header: $header";
 
         for (my $i = 0; $i < $num_reads; $i++) {
 
-            # determine fragment length according to the normal distribution:
+            # determine fragment length asccording to the normal distribution:
             my $frag_length = Math::CDF::qnorm(rand()) * $st_dev + $mean_frag_size;
             $frag_length = int($frag_length);
             
